9b3b67c08b4ee1e191036c0d78c86fd2ec3af4fe
seungtaek <seungtaek@seungtaeg-ui-MacBookPro.local>
2020-09-14 23:16:03 +0900

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write('<div>Platform Team Gazua</div>')
+  res.write(`<div>My name is ${process.env.MY_NAME}</div>`)
+  res.end()
+})
+
+app.listen(3000, () => {
+  console.log('server is listening on port 3000');
+})  
